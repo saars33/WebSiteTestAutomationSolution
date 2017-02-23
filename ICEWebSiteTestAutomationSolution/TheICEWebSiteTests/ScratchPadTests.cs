ff29fd8458ff779e4035c7dcd8341a1b7cbe5900
Sarthak <saars33@gmail.com>
2017-02-22 20:30:53 -0500

    the first commit to this project

---
+++ ICEWebSiteTestAutomationSolution/TheICEWebSiteTests/ScratchPadTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheICEWebSiteTests
+{
+    [TestClass]
+    public class ScratchPadTests
+    {
+        [TestInitialize]
+        public void MyScratchTestInitializeeMethod()
+        {
+            Console.WriteLine("inside MyScratchTestInitializeeMethod");    
+        }
+
+        [TestMethod]
+        public void MyFirstScratchTestMethod()
+        {
+            Console.WriteLine("inside MyFirstScratchTestMethod");
+        }
+
+        [TestCleanup]
+        public void MyScratchTestCleanup()
+        {
+            Console.WriteLine("inside MyScratchTestCleanup");
+        }
+
+    }
+}
